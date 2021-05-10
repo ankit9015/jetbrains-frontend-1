733a4d884d5fc9e48cabf3d8faea35b19640e91b
ankit9015 <67504902+ankit9015@users.noreply.github.com>
2021-05-10 23:04:51 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let popup = document.querySelector('.window');
+let navbar = document.querySelector('.cross');
+
+popopen = function() {
+    popup.style.display = 'block';
+};
+popclose = function() {
+    popup.style.display = 'none';
+};
+
+navopen = function() {
+    navbar.style.display = 'block';
+}
